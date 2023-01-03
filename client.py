fda932d96606dd92d86448f981e27a83831b398b
Parnal Patil <parnal.patil@gmail.com>
2023-01-03 23:28:50 +0530

    first commit

---
+++ client.py
@@ -0,0 +1,58 @@
+import socket
+import json
+import random
+import sys
+
+HEADER = 64
+PORT = 4063
+DISCONNECT_MESSAGE = "!DISCONNECT"
+# SERVER = socket.gethostbyname(socket.gethostname())
+SERVER = str(sys.argv[1])
+# ADDR = ("10.128.0.5", PORT)
+FORMAT = 'utf-8'
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# client.connect((SERVER, PORT))
+client.connect((SERVER, PORT))
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+connected = True
+while connected:
+    # inp = input("Enter 'set' to store data or 'get' to retrieve data: \n")
+    inputs = ['set cricket 4', 'get cricket']
+    inp = random.choice(inputs)
+    # inp = 'quit'
+    print(f"input is: {inp}")
+    split_val = inp.split()
+    kv_pair = {}
+    if split_val[0] == 'get':
+        send(split_val[1])
+    elif split_val[0] == 'set':
+
+        bytes_len = int(split_val[2])
+        val = 'bat'
+        # val = input()
+        final_val = val[0:bytes_len]
+        kv_pair[split_val[1]] = (final_val, bytes_len)
+        send(json.dumps(kv_pair))
+    elif split_val[0] == 'quit':
+        print('Quitting')
+        send(inp)
+        break
+        # connected = False
+    else:
+        print("Invalid input! Only 'get', 'set' or 'quit' as the first word is accepted")
+    send(DISCONNECT_MESSAGE)
+    break
+    
+
+# send(DISCONNECT_MESSAGE)
---
+++ server.py
@@ -0,0 +1,170 @@
+import socket 
+import threading
+import json
+import os
+import signal
+import sys
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import db
+
+
+HEADER = 64
+PORT = 4063
+# SERVER = "0.0.0.0"
+SERVER = socket.gethostbyname(socket.gethostname())
+print(f"SERVER is: {SERVER}")
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+TYPE = sys.argv[1]
+
+cred = credentials.Certificate("thermal-hour-367420-firebase-adminsdk-1p9ea-7e59909035.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(ADDR)
+
+
+def handle_client(conn, addr, type, client_num):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    print(f"Client: {client_num} connected.")
+    try:
+        if type == 'local':
+            connected = True
+            while connected:
+                msg_length = conn.recv(HEADER).decode(FORMAT)
+                print(f"msg_length: {msg_length}")
+                if msg_length:
+                    msg_length = int(msg_length)
+                    msg = conn.recv(msg_length).decode(FORMAT)
+                    print('new message is: ', msg)
+                    if msg == DISCONNECT_MESSAGE:
+                        connected = False
+
+                    elif msg == 'quit':
+                        connected = False
+
+                    elif len(msg.split()) > 1:
+                        # print(f"Before json loads: {msg}")
+                        new_msg = json.loads(msg)
+                        print(f"After json loads: {new_msg}")
+                        key_first = list(new_msg.keys())[0]
+                        val_first = new_msg[key_first][0]
+                        val_length_first = new_msg[key_first][1]
+                        print(f"Key: {key_first}, Value: {val_first}, Length: {val_length_first}")
+                        temp_dict = {}
+                        with open("memcache.txt", "r") as file:
+                            for line in file:
+                                temp_dict = json.loads(line)
+                                # print(f"temp_dict is: {temp_dict}")
+
+                        temp_dict[key_first] = (val_first, val_length_first)
+                        with open("memcache.txt", "w") as file:
+                            file.write(json.dumps(temp_dict))
+                        
+                        print(f"[{addr}] {new_msg}")
+                        conn.send("STORED".encode(FORMAT))
+
+                    elif len(msg.split()) == 1:
+                        print(f"Get raw message is: {msg}")
+                        new_msg = msg.split()[0]
+                        print(f"Get message is: {new_msg}")
+                        with open("memcache.txt", "r") as file:
+                            for line in file:
+                                temp_dict = json.loads(line)
+                        if new_msg in temp_dict.keys():
+                            conn.send(f"{new_msg} {temp_dict[new_msg][1]}\n{temp_dict[new_msg][0]}\nEND".encode(FORMAT))
+                        else:
+                            conn.send("-1".encode(FORMAT))
+
+        elif type == 'firebase':
+            connected = True
+            while connected:
+                msg_length = conn.recv(HEADER).decode(FORMAT)
+                print(f"msg_length: {msg_length}")
+                if msg_length:
+                    msg_length = int(msg_length)
+                    msg = conn.recv(msg_length).decode(FORMAT)
+                    print('new message is: ', msg)
+                    if msg == DISCONNECT_MESSAGE:
+                        connected = False
+
+                    elif msg == 'quit':
+                        connected = False
+
+                    elif len(msg.split()) > 1:
+                        # print(f"Before json loads: {msg}")
+                        new_msg = json.loads(msg)
+                        print(f"After json loads: {new_msg}")
+                        key_first = list(new_msg.keys())[0]
+                        val_first = new_msg[key_first][0]
+                        val_length_first = new_msg[key_first][1]
+                        print(f"Key: {key_first}, Value: {val_first}, Length: {val_length_first}")
+                        temp_dict = {}
+                        with open("memcache.txt", "r") as file:
+                            for line in file:
+                                temp_dict = json.loads(line)
+                                # print(f"temp_dict is: {temp_dict}")
+
+                        temp_dict[key_first] = (val_first, val_length_first)
+                        doc_ref = db.collection(u'KVStore').document('memcache')
+                        doc_ref.set(temp_dict)
+
+                        print(f"[{addr}] {new_msg}")
+                        conn.send("STORED".encode(FORMAT))
+
+                    elif len(msg.split()) == 1:
+                        print(f"Get raw message is: {msg}")
+                        new_msg = msg.split()[0]
+                        print(f"Get message is: {new_msg}")
+                        users_ref = db.collection(u'KVStore')
+                        docs = users_ref.stream()
+                        for doc in docs:
+                            temp_dict = doc.to_dict()
+                        if new_msg in temp_dict.keys():
+                            conn.send(f"{new_msg} {temp_dict[new_msg][1]}\n{temp_dict[new_msg][0]}\nEND".encode(FORMAT))
+                        else:
+                            conn.send("-1".encode(FORMAT))
+
+
+            # connected = False
+
+        # conn.close()
+        
+        
+        if not connected:
+            if msg == 'quit':
+                print(f"Client {client_num} disconnected!")
+                pid = os.getpid()
+                os.kill(pid, signal.SIGTERM)
+                conn.close()
+            else:
+                print(f"Client {client_num} disconnected!")
+                # pid = os.getpid()
+                # os.kill(pid, signal.SIGKILL)
+                conn.close()
+    except Exception as e:
+        print(f"error is: {e}")
+        print(f"Client {client_num} disconnected!")
+        # server.shutdown(socket.SHUT_RDWR)
+        conn.close()
+
+        
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr, TYPE, threading.activeCount()))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+print("[STARTING] server is starting...")
+start()
